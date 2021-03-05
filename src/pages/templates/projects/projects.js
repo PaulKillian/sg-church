e883d3b099be3b8608fc71b2560857ebceb0d046
PaulKillian <psk65lava@gmail.com>
2021-03-04 17:04:56 -0800

    changed to tailwinds

--- src/pages/templates/projects/projects.js
+++ src/pages/templates/projects/projects.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import '../hero/home.css'
 import { AiFillGithub } from 'react-icons/ai';
 import { CgWebsite } from "react-icons/cg";
+import { ProjectStyles, IconStyles, ImageStyles, RowStyles} from "./tailwind"
 
 
 function Projects() {
@@ -19,12 +20,14 @@ function Projects() {
 		const observer1 = new IntersectionObserver(entries => {
       entries.forEach(entry => {
 				if (entry.intersectionRatio > 0) {
+					entry.target.classList.remove('fade-node')
 					entry.target.classList.add('transition')
 				} else {
 					entry.target.classList.remove('transition')
+					entry.target.classList.add('fade-node')
 				}
 			});
-		});
+		}, {rootMargin: "0px 0px -200px 0px"});
 
 		fadeNodes.forEach(leftNode => {
 			observer1.observe(leftNode);
@@ -45,7 +48,6 @@ function Projects() {
 		});
 }
 
-
 	const openWindow = (event) => {
 		window.open(event.target.id, "width=200, height=1040, top=0, left=960");
 	}
@@ -57,22 +59,32 @@ function Projects() {
 	}
 
   return (
-    <div className={'d-flex flex-wrap main-color justify-content-center'}>
+    <div className={'main-color'}>
       {projects.map(project => {
 				return (
-          <div className={'d-flex pro-dimensions'} key={project.id}>
-            <img id={project.url}
-              onClick={openWindow} src={"http://localhost:1337" + project.images.url}
-							className='col-12 col-lg-6 m-auto project-initial-position img-dimension top slideNode'></img>
-            <div className={'text-justify top fade-node m-auto col-12 col-lg-5 p-5 parallax shadowed project-color'}>
+					<RowStyles key={project.id}>
+						<ImageStyles>
+							<img id={project.url} src={"http://localhost:1337" + project.images.url}></img>
+						</ImageStyles>
+            <ProjectStyles>
               <h3 className={'text-center pb-3 '}>{project.title}</h3>
 							<p>{project.descriptions}</p>
-							<div>
-							<AiFillGithub style={{color: '#D972FF'}} size={50}></AiFillGithub>
-							<CgWebsite style={{color: '#009999'}} size={50}></CgWebsite>
-						</div>
-						</div>
-          </div>
+							<div className={'d-flex align-items-center justify-content-center'}>
+								<div className={'rotate'}>
+									<IconStyles>
+										<AiFillGithub style={{ color: '#D972FF' }} size={30} />
+									</IconStyles>
+								</div>
+								<h6 className={'mr-3 text-shadow'}>CODE</h6>
+								<div className={'rotate'} onClick={openWindow}>
+									<IconStyles>
+										<CgWebsite id={project.url} style={{ color: '#009999' }} size={30} />
+									</IconStyles>
+								</div>
+								<h6 className={'text-shadow'}>LIVE</h6>
+							</div>
+						</ProjectStyles>
+          </RowStyles>
         )
         })}
   </div>
